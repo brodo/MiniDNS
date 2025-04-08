f6f2baad55e1ded5405dff9b0987c49fcc4f2c37
Julian Dax <julian.dax@posteo.de>
2025-04-08 18:06:18 +0200

    Revert "Refactor DNS server core to support resolver forwarding"
    This reverts commit fdf084a5d99a671f75c06ecf2e19b67868004521.

--- app/main.go
+++ app/main.go
@@ -1,74 +1,26 @@
 package main
 
 import (
+	"fmt"
 	"log/slog"
 	"net"
-	"os"
 )
 
 func main() {
-	port := "2053"
-	resolver := ""
-	debug := true
+	// todo: port can be set as a command line parameter
 
-	for i := 1; i < len(os.Args); i++ {
-		switch os.Args[i] {
-		case "--port":
-			if i+1 < len(os.Args) {
-				port = os.Args[i+1]
-				i++
-			}
-		case "--resolver":
-			if i+1 < len(os.Args) {
-				resolver = os.Args[i+1]
-				i++
-			}
-		}
-		if os.Args[i] == "--debug" {
-			debug = true
-		}
-	}
-
-	if debug {
-		lvl := new(slog.LevelVar)
-		lvl.Set(slog.LevelDebug)
-		logger := slog.New(slog.NewTextHandler(os.Stderr, &slog.HandlerOptions{
-			Level: lvl,
-		}))
-		slog.SetDefault(logger)
-		slog.Debug("Debug mode enabled")
-	}
-
-	udpAddr, err := net.ResolveUDPAddr("udp", "127.0.0.1:"+port)
+	udpAddr, err := net.ResolveUDPAddr("udp", "127.0.0.1:2053")
 	if err != nil {
-		slog.Error("Failed to resolve UDP address", "error", err)
+		fmt.Println("Failed to resolve UDP address:", err)
 		return
 	}
 
 	udpConn, err := net.ListenUDP("udp", udpAddr)
 	if err != nil {
-		slog.Error("Failed to bind to address", "error", err)
+		fmt.Println("Failed to bind to address:", err)
 		return
 	}
 	defer udpConn.Close()
-
-	var resolverAddr *net.UDPAddr
-	var resolverConn *net.UDPConn
-
-	if resolver != "" {
-		slog.Debug("Using resolver", "resolver", resolver)
-		resolverAddr, err = net.ResolveUDPAddr("udp", resolver)
-		if err != nil {
-			slog.Error("Failed to resolve resolver address", "error", err)
-			return
-		}
-		resolverConn, err = net.DialUDP("udp", nil, resolverAddr)
-		if err != nil {
-			slog.Error("Failed to bind to resolver address", "error", err)
-			return
-		}
-		defer resolverConn.Close()
-	}
 
 	buf := make([]byte, 512)
 
@@ -78,67 +30,14 @@ func main() {
 			slog.Error("Error receiving data", "error", err)
 			break
 		}
-		dnsMessage := DNSMessage(buf[:size])
-		slog.Debug("Received message", "size", size, "source", source, "dnsMessage", dnsMessage.String())
-		if resolverConn != nil {
-			msgBuf := make([]byte, 512)
-			questions, _ := dnsMessage.Questions()
-			answers := make([]Answer, 0)
-			rn := 0
-			for _, q := range questions {
-				copy(msgBuf, buf[:size])
-				forwardMsg := DNSMessage(msgBuf[:size])
-				pos := forwardMsg.SetQuestions([]Question{q})
-				slog.Debug("Forwarding dnsMessage to resolver", "resolver", resolver, "msg", forwardMsg.String())
-				_, err = resolverConn.Write(forwardMsg[:pos])
-				if err != nil {
-					slog.Error("Failed to forward dnsMessage", "error", err)
-				}
-				rn, err = resolverConn.Read(msgBuf)
-				if err != nil {
-					slog.Error("Error receiving data from resolver", "error", err)
-					return
-				}
-				dnsResponse := DNSMessage(msgBuf[:rn])
-				slog.Debug("Received response from resolver", "size", rn, "msg", dnsResponse.String())
-				_, n := dnsResponse.Questions()
-				respAnswers, err := dnsResponse.Answers(n)
-				if err != nil {
-					slog.Error("Failed to get answers", "error", err)
-					return
-				}
-				for _, answer := range respAnswers {
-					answers = append(answers, answer)
-				}
-			}
-			slog.Debug("Read questions", "questions", questions)
-			msg := DNSMessage(msgBuf[:rn])
-			slog.Debug("Building response", "msg", msg.String())
-			slog.Debug("setting questions", "questions", questions)
-			pos := msg.SetQuestions(questions)
-			slog.Debug("setting answers", "answers", answers)
-			size, err = msg.SetAnswers(answers, pos)
-			if err != nil {
-				slog.Error("Failed to set answers", "error", err)
-				return
-			}
+		packet := Packet(buf[:size])
+		slog.Info("Received message", "size", size, "source", source, "packet", packet.String())
+		questions, n := packet.Questions()
+		slog.Info("Question section", "questions", questions)
+		packet.SetIsResponse()
 
-			slog.Debug("Sending response", "response", msg.String())
-
-			_, err = udpConn.WriteToUDP(msgBuf[:size], source)
-			if err != nil {
-				slog.Error("Failed to forward dnsMessage", "error", err)
-				return
-			}
-			continue
-		}
-
-		questions, n := dnsMessage.Questions()
-		slog.Debug("Question section", "questions", questions)
-		dnsMessage.SetIsResponse()
-
-		if dnsMessage.Opcode() != 0 {
-			dnsMessage.SetResponseCode(4)
+		if packet.Opcode() != 0 {
+			packet.SetResponseCode(4)
 		}
 
 		answers := make([]Answer, 0)
@@ -152,17 +51,17 @@ func main() {
 			}
 			answers = append(answers, answer)
 		}
-		size, err = dnsMessage.SetAnswers(answers, n)
+		err = packet.SetAnswers(answers, n)
 		if err != nil {
-			slog.Debug("Failed to set answers", "error", err)
+			slog.Info("Failed to set answers", "error", err)
 		}
 
-		slog.Debug("Sending response", "response", string(dnsMessage))
-		slog.Debug("Sending message", "response", dnsMessage.String())
+		slog.Info("Sending response", "response", string(packet))
+		slog.Info("Sending message", "response", packet.String())
 
-		_, err = udpConn.WriteToUDP(dnsMessage, source)
+		_, err = udpConn.WriteToUDP(packet, source)
 		if err != nil {
-			slog.Debug("Failed to send response", "error", err)
+			slog.Info("Failed to send response", "error", err)
 		}
 	}
 }
